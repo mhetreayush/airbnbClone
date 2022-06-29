688093c3429a37433227df488ec9f9f94622aa40
Ayush Mhetre <mhetreayush1719@gmail.com>
2022-06-29 10:45:52 +0530

    text-color fixed

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,15 +1,15 @@
 import photoGrid from "../../images/photoGrid.png"
 export default function Hero(){
     return(
-        <div className="flex flex-col items-center text-center text-black">
+        <div className="flex flex-col items-center text-center text-black dark:text-white">
             <div>
             <img className="mb-4" src={photoGrid} />
             </div>
             <div className="my-5">
-            <h1 className='text-3xl font-extrabolddark:text-white'>
+            <h1 className='text-3xl font-extrabold'>
                 Online Experiences
             </h1>
-            <p className='dark:text-white'>
+            <p>
             Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.
             </p>
             </div>
