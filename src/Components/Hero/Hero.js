de24ae8d7e941259daa285b4bea0fbe7bdea9115
Ayush Mhetre <mhetreayush1719@gmail.com>
2022-06-29 10:43:44 +0530

    text-black added

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,15 +1,15 @@
 import photoGrid from "../../images/photoGrid.png"
 export default function Hero(){
     return(
-        <div className="flex flex-col items-center text-center">
+        <div className="flex flex-col items-center text-center text-black">
             <div>
             <img className="mb-4" src={photoGrid} />
             </div>
             <div className="my-5">
-            <h1 className='text-3xl font-extrabold text-black dark:text-white'>
+            <h1 className='text-3xl font-extrabolddark:text-white'>
                 Online Experiences
             </h1>
-            <p className='text-black dark:text-white'>
+            <p className='dark:text-white'>
             Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.
             </p>
             </div>
